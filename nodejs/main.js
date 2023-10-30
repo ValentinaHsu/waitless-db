9c324d4048786024605c07619e995b334d66338a
VELO BARES ABRIL <47206356@ort>
2023-10-30 15:14:14 -0300

    a

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,7 +1,20 @@
 import bodyParser from 'body-parser';
 import express from 'express';
 import cors from "cors"
-import { getAllFoodWithPrisma, getFoodWithPrisma, createFoodWithPrisma, updateFoodWithPrisma, deleteFoodWithPrisma, addFoodToOrder, getAllOrderWithPrisma, getAllCommandWithPrisma, getCommandWithPrismaByID, getCommandWithPrismaByTable, createCommandWithPrisma, deleteCommandWithPrisma, getAllOrderFoodByCustumerWithPrisma, getOrderFoodByCustumerByPrismaID } from '../index.js'
+import { getAllFoodWithPrisma, 
+    getFoodWithPrisma, 
+    createFoodWithPrisma, 
+    updateFoodWithPrisma, 
+    deleteFoodWithPrisma, 
+    addFoodToOrder, 
+    getAllOrderWithPrisma, 
+    getAllCommandWithPrisma, 
+    getCommandWithPrismaByID, 
+    getCommandWithPrismaByTable, 
+    createCommandWithPrisma, 
+    deleteCommandWithPrisma, 
+    getAllOrderFoodByCustumerWithPrisma, 
+    getOrderFoodByCustumerByPrismaID } from '../index.js'
 const app = express();
 const PORT = 3002;
 
